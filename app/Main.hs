c690333eb46c885e1c281626bf7dbdce29700901
shokohara <shokohara@users.noreply.github.com>
2016-09-30 12:47:50 +0900

    Refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,14 @@ module Main where
 
 import Control.Lens
 
-seedList = [0..100]
-effector = [1..3]
-f :: [Int] -> [[Int]] -> [[Int]]
-f [] n = n
-f l n = f (drop 1 l) $ n ++ [take 3 l]
-productTuple :: (Int, Int) -> Int
-productTuple v = product $ (^..each) v
 main = do
-  let x = f seedList [] :: [[Int]]
-  let y = map z x :: [[(Int, Int)]]
-  let r = map sum $ map (map productTuple) y :: [Int]
-  print r
+  let seedList = [0..100]
+  let effector = [1..3]
+  print $ map sum $ map (map productTuple) $ map (zip effector) $ f seedList []
     where
-      z :: [Int] -> [(Int, Int)]
-      z v = zip v effector
+      f :: [Int] -> [[Int]] -> [[Int]]
+      f [] n = n
+      f l n = f (drop 1 l) (n ++ [take 3 l])
+      productTuple :: (Int, Int) -> Int
+      productTuple v = product $ (^..each) v
 
