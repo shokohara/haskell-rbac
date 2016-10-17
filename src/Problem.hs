47609e1925d4042da5e80eba5e5a24481c659ee9
shokohara <shokohara@users.noreply.github.com>
2016-10-17 12:44:54 +0900

    Problem2

--- src/Problem.hs
+++ src/Problem.hs
@@ -7,3 +7,9 @@ problem1 = p1 1000
 
 p1 n = sum [ x | x <- [1..n-1], x `mod` 3 == 0 || x `mod` 5 == 0]
 
+problem2 :: Integer
+problem2 = sum . filter even . takeWhile (<=4000000) $ fibs
+
+fib n = fibs!!n
+fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,4 +9,8 @@ main = hspec $ do
     p1 10 `shouldBe` 23
   describe "problem1" $ it "" $
     problem1 `shouldBe` 233168
+  describe "fib" $ it "" $
+    map fib [0..9] `shouldBe` [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  describe "problem2" $ it "" $
+    problem2 `shouldBe` 4613732
 
